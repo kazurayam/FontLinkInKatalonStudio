f2f48ca8a286e362600744a3c1628c82ff52907e
kazurayam <kazuaki.urayama@gmail.com>
2019-02-28 06:35:39 +0900

    first commit

---
+++ Scripts/TC1/Script1551302560537.groovy
@@ -0,0 +1,27 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+/**
+ * Test Cases/TC1
+ * 
+ * Reproducing an I18N problem in Katalon Studio.
+ * Trying to fix it by Font Link.
+ * 
+ * This test case emits 2 messages in the console. One requires Cylil font, another requires CJK font.
+ *     Здравствуйте
+ *     こんにちは
+ */
+WebUI.comment('Здравствуйте')
+WebUI.comment('こんにちは')
